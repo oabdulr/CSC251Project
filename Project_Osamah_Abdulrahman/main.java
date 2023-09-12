f2a1875e003751ab66611e8a38ba8c9c82100c75
oabdulr <144569041+oabdulr@users.noreply.github.com>
2023-09-11 22:30:01 -0400

    Update main.java

--- Project_Osamah_Abdulrahman/main.java
+++ Project_Osamah_Abdulrahman/main.java
@@ -2,7 +2,10 @@ import java.io.*;
 import java.util.Scanner;
 import InsurancePolicy;
 
-class Main {
+
+
+
+class main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
@@ -37,22 +40,21 @@ class Main {
 
       InsurancePolicy newPolicyHolder = new InsurancePolicy(policyNum, providerName, firstName, lastName, age, isSmoker, height, weight);
 
-      System.out.println("Policy Number: " + newPolicyHolder.);
-
-      /*
-      Please enter the Provider Name: State Farm
-
-Please enter the Policyholder’s First Name: John
-
-Please enter the Policyholder’s Last Name: Doe
-
-Please enter the Policyholder’s Age: 24
-
-Please enter the Policyholder’s Smoking Status (smoker/non-smoker): non-smoker
-
-Please enter the Policyholder’s Height (in inches): 62.0
-
-Please enter the Policyholder’s Weight (in pounds): 250.5*/
+      System.out.println("Policy Number: " + newPolicyHolder.get_policyNumber());
+      System.out.println("Provider Name: " + newPolicyHolder.get_providersName());
+      System.out.println("Policyholder’s First Name: " + newPolicyHolder.get_holderFirstName());
+      System.out.println("Policyholder’s Last Name: " + newPolicyHolder.get_holderLastName());
+      System.out.println("Policyholder’s Age: " + newPolicyHolder.get_holdersAge());
+      if (newPolicyHolder.get_isHolderSmoker()){
+         System.out.println("Policyholder’s Smoking Status: smoker");
+      }
+      else{
+         System.out.println("Policyholder’s Smoking Status: non-smoker");
+      }
       
+      System.out.println("Policyholder’s Height: " + newPolicyHolder.get_holderHeightIn());
+      System.out.println("Policyholder’s Weight: " + newPolicyHolder.get_holderWeightLb());
+      System.out.println("Policyholder’s BMI: " + newPolicyHolder.CalculateBMI());  
+      System.out.println("Policy Price: " + newPolicyHolder.PolicyPrice());  
   }
 }
