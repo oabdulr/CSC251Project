c6c2a3119e2a087abc878f6aae397a435ed0a930
oabdulr <144569041+oabdulr@users.noreply.github.com>
2023-10-02 19:29:40 -0400

    Update main.java

--- Project_Osamah_Abdulrahman/main.java
+++ Project_Osamah_Abdulrahman/main.java
@@ -1,60 +1,49 @@
 import java.io.*;
 import java.util.Scanner;
-import InsurancePolicy;
-
-
-
+import java.util.ArrayList;
 
 class main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  public static ArrayList<InsurancePolicy> InsurancePolices = new ArrayList<InsurancePolicy>();
 
-      System.out.print("Please enter the Policy Number: ");
-      int policyNum = sc.nextInt();
+  public static void Main(String[] args) throws FileNotFoundException {
+      File file = new File("PolicyInformation.txt");
+      Scanner sc = new Scanner(file);
+        System.out.println("S");
+      while (sc.hasNext()) {
+        int policyNum = sc.nextInt();
+        sc.nextLine();
+        String providerName = sc.nextLine();
+        String firstName = sc.nextLine();
+        String lastName = sc.nextLine();
+        int age = sc.nextInt();
+        sc.nextLine();
+        boolean isSmoker = sc.nextLine().equalsIgnoreCase("smoker");
+        int height = sc.nextInt();
+        int weight = sc.nextInt();
+        System.out.println("Added Mr."+lastName);
 
-      System.out.print("Please enter the Provider Name: ");
-      String providerName = sc.nextLine();
-
-      System.out.print("Please enter the Policyholder’s First Name: ");
-      String firstName = sc.nextLine();
-      
-      System.out.print("Please enter the Policyholder’s Last Name: ");
-      String lastName = sc.nextLine();
-      
-      System.out.print("Please enter the Policyholder’s Age: ");
-      int age = sc.nextInt();
-
-      System.out.print("Please enter the Policyholder’s Smoking Status (smoker/non-smoker): ");
-      String smoker_temp = sc.nextLine();
-      
-      boolean isSmoker = false;
-      if (smoker_temp.equalsIgnoreCase("smoker")){
-          isSmoker = true;
+        InsurancePolicy newPolicyHolder = new InsurancePolicy(policyNum, providerName, firstName, lastName, age, isSmoker, height, weight);
+        InsurancePolices.add(newPolicyHolder);
       }
 
-      System.out.print("Please enter the Policyholder’s Height (in inches): ");
-      double height = sc.nextDouble();
-
-      System.out.print("Please enter the Policyholder’s weight (in pounds): ");
-      double weight = sc.nextDouble();
-
-      InsurancePolicy newPolicyHolder = new InsurancePolicy(policyNum, providerName, firstName, lastName, age, isSmoker, height, weight);
+      /*
 
       System.out.println("Policy Number: " + newPolicyHolder.get_policyNumber());
       System.out.println("Provider Name: " + newPolicyHolder.get_providersName());
-      System.out.println("Policyholder’s First Name: " + newPolicyHolder.get_holderFirstName());
-      System.out.println("Policyholder’s Last Name: " + newPolicyHolder.get_holderLastName());
-      System.out.println("Policyholder’s Age: " + newPolicyHolder.get_holdersAge());
+      System.out.println("Policyholder's First Name: " + newPolicyHolder.get_holderFirstName());
+      System.out.println("Policyholder's Last Name: " + newPolicyHolder.get_holderLastName());
+      System.out.println("Policyholder's Age: " + newPolicyHolder.get_holdersAge());
       if (newPolicyHolder.get_isHolderSmoker()){
-         System.out.println("Policyholder’s Smoking Status: smoker");
+         System.out.println("Policyholder's Smoking Status: smoker");
       }
       else{
-         System.out.println("Policyholder’s Smoking Status: non-smoker");
-      }
+         System.out.println("Policyholder's Smoking Status: non-smoker");
+      } // osamah bin laden
       
-      System.out.println("Policyholder’s Height: " + newPolicyHolder.get_holderHeightIn());
-      System.out.println("Policyholder’s Weight: " + newPolicyHolder.get_holderWeightLb());
-      System.out.println("Policyholder’s BMI: " + newPolicyHolder.CalculateBMI());  
+      System.out.println("Policyholder's Height: " + newPolicyHolder.get_holderHeightIn());
+      System.out.println("Policyholder's Weight: " + newPolicyHolder.get_holderWeightLb());
+      System.out.println("Policyholder's BMI: " + newPolicyHolder.CalculateBMI());  
       System.out.println("Policy Price: " + newPolicyHolder.PolicyPrice());  
+       */
   }
 }
