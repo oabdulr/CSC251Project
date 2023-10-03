3cb168287df674c4c4a2b19dd513133b46d1f5ad
oabdulr <144569041+oabdulr@users.noreply.github.com>
2023-10-02 22:11:57 -0400

    Update
    Modified to read input from a file

--- Project_Osamah_Abdulrahman/InsurancePolicy.java
+++ Project_Osamah_Abdulrahman/InsurancePolicy.java
@@ -11,65 +11,143 @@ public class InsurancePolicy {
     private double holderHeightIn;
     private double holderWeightLb;
 
+    /**
+     * @return double the policy holders weight
+     */
+
     public double get_holderWeightLb(){
         return this.holderWeightLb;
     }
+
+    /**
+     * @param value the new weight for the policy holder
+     */
 
     public void set_holderWeightLb(double value){
         this.holderWeightLb = value;
     }
     
+    /**
+     * 
+     * @return double get the polciy holders height
+     */
+
     public double get_holderHeightIn(){
         return this.holderHeightIn;
     }
+
+    /**
+     * 
+     * @param value the new policy holders weight
+     */
 
     public void set_holderHeightIn(double value){
         this.holderHeightIn = value;
     }
     
+    /**
+     * 
+     * @return boolean returns the policy holders smoker status
+     */
+
     public boolean get_isHolderSmoker(){
         return this.isHolderSmoker;
     }
+
+    /**
+     * 
+     * @param smoker sets the policy holders smoker status
+     */
 
     public void set_isHolderSmoker(boolean smoker){
         this.isHolderSmoker = smoker;
     }
     
+    /**
+     * 
+     * @return returns the policy holders age
+     */
+
     public int get_holdersAge(){
         return this.holdersAge;
     }
+
+    /**
+     * 
+     * @param age sets the policy holders age
+     */
 
     public void set_holdersAge(int age){
         this.holdersAge = age;
     }
 
+    /**
+     * 
+     * @param name sets the policy holders name
+     */
+
     public void set_holderLastName(String name){
         this.holderLastName = name;
     }
+
+    /**
+     * 
+     * @return returns the policy holders name
+     */
 
     public String get_holderLastName(){
         return this.holderLastName;
     }
     
+    /**
+     * 
+     * @param number sets the policy number
+     */
+
     public void set_policyNumber(int number){
         this.policyNumber = number;
     }
+
+    /**
+     * 
+     * @return returns the policy number
+     */
 
     public int get_policyNumber(){
         return this.policyNumber;
     }
 
+    /**
+     * 
+     * @return gets the providers name
+     */
+
     public String get_providersName(){
         return this.providersName;
     }
+
+    /**
+     * 
+     * @param name sets the proveiders name
+     */
     
     public void set_providersName(String name){
         this.providersName = name;
     }
 
+    /**
+     * 
+     * @param name sets the policy holders first name
+     */
+
     public void set_holderFirstName(String name){
         this.holderFirstName = name;
     }
+
+    /**
+     * 
+     * @return returns the policy holders first name
+     */
 
     public String get_holderFirstName(){
         return this.holderFirstName;
@@ -97,9 +175,19 @@ public class InsurancePolicy {
         this.holderWeightLb = holderWeightLb;
     }
 
+    /**
+     * 
+     * @return returns the BMI for the policy holder
+     */
+
     public double CalculateBMI(){
         return (this.holderWeightLb * 703) / (this.holderHeightIn * this.holderHeightIn);
     }
+
+    /**
+     * 
+     * @return calcualte and returns the policy holders price
+     */
 
     public double PolicyPrice(){
         double price = 0.00;
--- Project_Osamah_Abdulrahman/main.java
+++ Project_Osamah_Abdulrahman/main.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 class main {
   public static ArrayList<InsurancePolicy> InsurancePolices = new ArrayList<InsurancePolicy>();
 
-  public static void Main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws FileNotFoundException {
       File file = new File("PolicyInformation.txt");
       Scanner sc = new Scanner(file);
-        System.out.println("S");
+
+      int totalSmoker = 0;
+      
       while (sc.hasNext()) {
         int policyNum = sc.nextInt();
         sc.nextLine();
@@ -20,30 +22,33 @@ class main {
         boolean isSmoker = sc.nextLine().equalsIgnoreCase("smoker");
         int height = sc.nextInt();
         int weight = sc.nextInt();
-        System.out.println("Added Mr."+lastName);
-
         InsurancePolicy newPolicyHolder = new InsurancePolicy(policyNum, providerName, firstName, lastName, age, isSmoker, height, weight);
         InsurancePolices.add(newPolicyHolder);
+
+        System.out.println("Policy Number: " + newPolicyHolder.get_policyNumber());
+        System.out.println("Provider Name: " + newPolicyHolder.get_providersName());
+        System.out.println("Policyholder's First Name: " + newPolicyHolder.get_holderFirstName());
+        System.out.println("Policyholder's Last Name: " + newPolicyHolder.get_holderLastName());
+        System.out.println("Policyholder's Age: " + newPolicyHolder.get_holdersAge());
+        if (newPolicyHolder.get_isHolderSmoker()){
+          System.out.println("Policyholder's Smoking Status: smoker");
+          totalSmoker++;
+        }
+        else{
+          System.out.println("Policyholder's Smoking Status: non-smoker");
+        }
+        
+        System.out.println("Policyholder's Height: " + newPolicyHolder.get_holderHeightIn());
+        System.out.println("Policyholder's Weight: " + newPolicyHolder.get_holderWeightLb());
+        System.out.printf("Policyholder's BMI: %.2f\n", newPolicyHolder.CalculateBMI());  
+        System.out.printf("Policy Price: $%,.2f\n", newPolicyHolder.PolicyPrice());  
+        System.out.println();
+       
       }
 
-      /*
 
-      System.out.println("Policy Number: " + newPolicyHolder.get_policyNumber());
-      System.out.println("Provider Name: " + newPolicyHolder.get_providersName());
-      System.out.println("Policyholder's First Name: " + newPolicyHolder.get_holderFirstName());
-      System.out.println("Policyholder's Last Name: " + newPolicyHolder.get_holderLastName());
-      System.out.println("Policyholder's Age: " + newPolicyHolder.get_holdersAge());
-      if (newPolicyHolder.get_isHolderSmoker()){
-         System.out.println("Policyholder's Smoking Status: smoker");
-      }
-      else{
-         System.out.println("Policyholder's Smoking Status: non-smoker");
-      } // osamah bin laden
+      System.out.println("The number of policies with a smoker is: " + totalSmoker);
+      System.out.println("The number of policies with a non-smoker is: " + (InsurancePolices.size() - totalSmoker));
       
-      System.out.println("Policyholder's Height: " + newPolicyHolder.get_holderHeightIn());
-      System.out.println("Policyholder's Weight: " + newPolicyHolder.get_holderWeightLb());
-      System.out.println("Policyholder's BMI: " + newPolicyHolder.CalculateBMI());  
-      System.out.println("Policy Price: " + newPolicyHolder.PolicyPrice());  
-       */
   }
 }
